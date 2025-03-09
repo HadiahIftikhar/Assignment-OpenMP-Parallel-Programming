27fbe31a6d5b4f3e2e377c0740e86dd57e0e88a2
HadiahIftikhar <hadiahiftikharntucs@gmail.com>
2025-03-09 09:04:00 +0500

    Created the Histogram Equalization Function which first calculates the histogram, then calculates the cumulative ditribution function, normalizes the CDF vaues and finally maps each pixel to its normalized CDF value in the output image.

--- Assignment1.c
+++ Assignment1.c
@@ -4,8 +4,8 @@
 #include <time.h>
 
 //defining the width and height of the image in pixels that will be enhanced, created a 5x5 image
-#define width 5
-#define height 5
+#define width 5     //number of columns
+#define height 5    //number of rows
 
 //hardcoding the pixel values for the image
 int image[height][width] = {
@@ -14,4 +14,37 @@ int image[height][width] = {
     {68, 61, 77, 87, 94},
     {70, 77, 93, 98, 93},
     {69, 78, 96, 93, 87}
+};
+
+//creating the histogram equalization function
+void HistogramEqualization(int img[height][width], int output_img[height][width]){
+    int histogram[256] = {0}; //created a 1D array to represent the image's histogram ad store the frequency of each pixel value from 0-255, initialized to 0
+    int cdf[256] = {0}; //1D array to hold the values resulting from the cumulative distribution function for each pixel value
+    int total_pixels = width * height;
+
+    //Step 1: filling the histogram array with pixel values
+    for (int i = 0; i < height; i++){
+        for (int j = 0; j < width; j++){
+            histogram[img[i][j]]++;   //traversing through the image matrix and incrementing the value of the corresponding index in the histogram for each pixel value in the matrix
+        }
+    }
+
+    //step 2: Calculating the CDF
+    cdf[0] = histogram[0];
+    for (int i = 1; i<256; i++){
+        cdf[i] =cdf[i-1] + histogram[i];    //calculating the CDF value for each intensity value, thus calculating the cdf sum for the histogram
+    }
+
+    //Step 3: normalizing the cdf in the --255 range
+    for (int i=0; i<256; i++){
+        cdf[i] = (cdf[i] - cdf[0]) * 255 / (total_pixels-1);
+    }
+
+    //step 4: Mapping the normalized values to the respective pixel using the output image
+    for (int i=0; i<height; i++){
+        for (int j=0; j<width; j++){
+            output_img[i][j] = cdf[img[i][j]];
+        }
+    }
+
 }
