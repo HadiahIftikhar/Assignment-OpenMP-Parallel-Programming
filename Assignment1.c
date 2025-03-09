3d523c6f81d9d857f33172b60614dce08beb5c57
HadiahIftikhar <hadiahiftikharntucs@gmail.com>
2025-03-09 09:51:24 +0500

    Wrote the main function, created the 2D array for the output image, passed the original and output image to the histogram equalization function and calculated the execution time taken by it to perform equalization, and print the results

--- Assignment1.c
+++ Assignment1.c
@@ -47,4 +47,35 @@ void HistogramEqualization(int img[height][width], int output_img[height][width]
         }
     }
 
+    //displaying the original image, the output image after histogram equalization was performed and the execution time taken for the equalization function
+    printf("Original Image:\n");
+    for(int i =0; i<height; i++){
+        for (int j = 0; j<width; j++){
+            printf("%d ", image[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nEqualized Image:\n");
+    for(int i = 0; i<height; i++){
+        for (int j = 0; j<width; j++){
+            printf("%d ", output_img[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int output_img[height][width];
+    struct timespec start, end;
+
+    clock_gettime(CLOCK_MONOTONIC, &start); //calculating the starting time of the execution
+    HistogramEqualization(image, output_img);
+    clock_gettime(CLOCK_MONOTONIC, &end);   //calculating the ending time of the execution
+
+    double execution_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+
+    printf("\nExecution Time: %f seconds\n", execution_time);
+
+    return 0;
 }
